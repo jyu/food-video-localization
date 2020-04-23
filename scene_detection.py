9dd39d0d7868d86956d11c8aad86280f6b6044bb
jyu <jerryyu8@gmail.com>
2020-04-23 00:24:26 +0000

    Add scene detection experiments

--- scene_detection.py
+++ scene_detection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[1]:
 
 
 from sklearn import preprocessing
@@ -11,12 +11,23 @@ import cv2
 import numpy as np
 import os
 import random
-from keras import models, optimizers, layers
-from keras.models import load_model
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import tensorflow as tf
+from tensorflow.keras import models, optimizers
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+import gc
 
 
-# In[5]:
+# In[2]:
+
+
+print(tf.__version__)
+print(tf.config.list_physical_devices('GPU'))
+
+
+# In[3]:
 
 
 labels = os.listdir("labels")
@@ -24,21 +35,12 @@ names = []
 for l in labels:
     names.append(l.replace(".json", ""))
 
-test_names = random.choices(names, k=4)
-train_names = []
-for name in names:
-    if not name in test_names:
-        train_names.append(name)
 
-print("train", train_names)
-print("test", test_names)
-
-
-# In[6]:
+# In[4]:
 
 
 def load_data_from_video(name):
-    print('cnn/' + name + '.feat')
+#     print('cnn/' + name + '.feat')
     cnn_array = np.genfromtxt('cnn/' + name + '.feat', delimiter=";")
     scene_f = open('scene_labels/' + name + '.labels')
     labels = scene_f.readlines()
@@ -46,7 +48,7 @@ def load_data_from_video(name):
     for l in labels:
         scene_array.append(l.replace("\n",""))
         
-    print("cnn array dim", cnn_array.shape, "scene array dim", len(scene_array))
+#     print("cnn array dim", cnn_array.shape, "scene array dim", len(scene_array))
     # Make sure data is same shape as label
     assert(cnn_array.shape[0] == len(scene_array))
     
@@ -58,7 +60,7 @@ def load_data_from_video(name):
         feat = cnn_array[i]
         feat = preprocessing.scale(feat)
         X.append(feat)
-        if scene_array[i] == "scene":
+        if scene_array[i] in ["scene", "miniscene"]:
             Y.append(1)
         else:
             Y.append(0)
@@ -68,217 +70,166 @@ def load_data_from_video(name):
     return (X, Y)
 
 
-# In[7]:
-
-
-train_X, train_Y = [], []
-for train_name in train_names:
-    X, Y = load_data_from_video(train_name)
-    train_X += X
-    train_Y += Y
-
-train_X = np.array(train_X)
-train_Y = np.array(train_Y)
-
-
-# In[8]:
-
-
-test_X, test_Y = [], []
-for test_name in test_names:
-    X, Y = load_data_from_video(test_name)
-    test_X += X
-    test_Y += Y
-
-test_X = np.array(test_X)
-test_Y = np.array(test_Y)
-
-
-# In[9]:
-
-
-print('train X shape', train_X.shape)
-print('train Y shape', train_Y.shape)
-
-print('test X shape', test_X.shape)
-print('test Y shape', test_Y.shape)
-
-
-# In[ ]:
-
-
-Cs = [0.1, 1, 10, 100, 1000]
-# kernels = ['poly', 'rbf', 'sigmoid']
-kernels = ['rbf']
-
-params = []
-scores = []
-models = []
-for C in Cs:
-    for kernel in kernels:
-        gamma = 'scale'
-        model = SVC(gamma=gamma, probability=True, C=C, kernel=kernel)
-        print("Params C:", C, "kernel:", kernel)
-        model.fit(train_X, train_Y)
-        print("Train score", model.score(train_X, train_Y))
-        test_score = model.score(test_X, test_Y)
-        print("Test score", test_score)
-        params.append({'C': C, 'kernel': kernel})
-        scores.append(test_score)
-        models.append(model)
-
-
-# In[31]:
-
-
-# LSTM?
-# neural network?
-
-model = models.Sequential()
-model.add(layers.Dense(16, input_dim=2048, activation='relu'))
-model.add(layers.Dropout(0.25))
-model.add(layers.BatchNormalization())
-model.add(layers.Dense(16, activation='sigmoid'))
-model.add(layers.Dropout(0.25))
-model.add(layers.BatchNormalization())
-model.add(layers.Dense(1, activation='sigmoid'))
-model.summary()
-
-mc = ModelCheckpoint('models/best_scene_nn.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
-
-model.compile(loss='binary_crossentropy', 
-              optimizer='adam',
-              metrics=['accuracy'])
-history = model.fit(x=train_X, 
-                    y=train_Y,
-                    validation_data=(test_X, test_Y),
-                    epochs=20, 
-                    batch_size=32,
-                    callbacks=[mc]
-                   )
-
-
-# In[32]:
-
-
-# Save model
-model.save('models/scene_nn.h5')
-
-
-# In[2]:
-
-
-model = load_model('models/scene_nn.h5')
-
-
-# In[33]:
-
-
-def test_on_video(name, model):
-    cnn_array = np.genfromtxt('cnn/' + name + '.feat', delimiter=";")
-    scene_f = open('scene_labels/' + name + '.labels')
-    labels = scene_f.readlines()
-    scene_array = []
-    for l in labels:
-        scene_array.append(l.replace("\n",""))
-    
-    for i in range(len(scene_array)):
-        feat = cnn_array[i]
-        feat = preprocessing.scale(feat)
-        feat = np.array([feat])
-        pred = model.predict(feat)[0]
-        pred_label = "transition"
-        if pred >= 0.5:
-            pred_label = "scene"
-        print(pred_label.ljust(10), scene_array[i].ljust(10))
-
-test_on_video('2_Egg_Vs_95_Egg', model)
-    
-
-
-# In[ ]:
-
-
-3_Seafood_Vs_213_Seafood_•_Australia
-test_on_video('2_Egg_Vs_95_Egg', model)
-
-
-# In[8]:
-
-
-def run_on_video(name, model):
-    cnn_array = np.genfromtxt('cnn/' + name + '.feat', delimiter=";")
-    video_cap = cv2.VideoCapture('data/' + name + '.mp4')
-    fps = video_cap.get(cv2.CAP_PROP_FPS)
-
-    preds = []
-    for i in range(len(cnn_array)):
-        feat = cnn_array[i]
-        feat = preprocessing.scale(feat)
-        feat = np.array([feat])
-        pred = model.predict(feat)[0]
-        pred_label = "transition"
-        if pred >= 0.5:
-            pred_label = "scene"
-        preds.append(pred_label)
-    return preds, fps
-
-
 # In[5]:
 
 
-# Post processing into timestamps of scenes
-def get_scene_transitions(seq):
-    prev_label = seq[0]   
-    transitions = []
-    for i in range(len(seq)):
-        label = seq[i]
-        
-        # Transition
-        if label != prev_label:
-            # Check if real transition: before has to be same, after has to be same
-            # 10 key frames -> 100 frames -> ~3s
-            check_const = 10
-            same = True
-            for j in range(1,check_const):
-                same = same and seq[i-1] == seq[i-j] and seq[i] == seq[i+j]
+video_to_data = {}
+i = 0
+for name in names:
+    i += 1
+    X, Y = load_data_from_video(name)
+    video_to_data[name] = (X, Y)
+    print(str(i) + '/' + str(len(names)))
 
-            if same:
-                transitions.append((i, label))
-        prev_label = label
+
+# In[6]:
+
+
+def get_model(params, suffix):
+    hidden_units = params['hidden_units']
+    use_dropout = params['use_dropout']
+    use_batch_norm = params['use_batch_norm']
+    num_layers = params['layers']
+
+    model = models.Sequential()
+    model.add(Dense(hidden_units, input_dim=2048, activation='relu'))
+    if use_dropout:
+        model.add(Dropout(0.25))
+    if use_batch_norm:
+        model.add(BatchNormalization())
     
-    return transitions
+    for i in range(num_layers - 1):
+        model.add(Dense(hidden_units, activation='relu'))
+        if use_dropout:
+            model.add(Dropout(0.25))
+        if use_batch_norm:
+            model.add(BatchNormalization())
+            
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
+
+    mc = ModelCheckpoint('models/scene_nn_' + suffix + '.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+    model.compile(loss='binary_crossentropy', 
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model, mc, model.count_params()
 
 
 # In[10]:
 
 
-def get_scene_timestamps(transitions, fps):
-    scenes = []
-    framestamps = []
-    for i in range(len(transitions) - 1):
-        transition = transitions[i]
-        next_transition = transitions[i+1]
-        if transition[1] == 'scene' and next_transition[1] == 'transition':
-            scenes.append((transition[0] * 10 / fps, next_transition[0] * 10 / fps))
-            framestamps.append((transition[0], next_transition[0]))
-    # Get first three scenes
-    scenes = scenes[:3]
-    framestamps = framestamps[:3]
-    return scenes, framestamps
+class CollectCallback(Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    gc.collect()
+
+    
+def train_nn(params, suffix, train_X, train_Y, test_X, test_Y):
+    model, mc, size = get_model(params, suffix)
+    save = params['save']
+    callbacks = [CollectCallback()]
+    if save:
+        callbacks.append(mc)
+        
+    history = model.fit(x=train_X, 
+                    y=train_Y,
+                    validation_data=(test_X, test_Y),
+                    epochs=30, 
+                    batch_size=32,
+                    callbacks=callbacks,
+                    verbose=1,
+                   )
+    val_max = max(history.history['val_accuracy'])
+    del model
+#     i = np.argmax(np.array(history.history['val_acc']))
+#     train_max = history.history['accuracy'][i]
+    return val_max, size
 
 
 # In[11]:
 
 
-def get_timestamps_from_video(name, model):
-    preds, fps = run_on_video(name, model)
-    transitions = get_scene_transitions(preds)
-    print(transitions)
-    timestamps, framestamps = get_scene_timestamps(transitions, fps)
-    print(timestamps)
-    print(framestamps)
-    return timestamps
+def cross_validate_method(train_fn, params):
+        
+    # Cross validation
+    k = 5
+    random.shuffle(names)
+    batches = len(names) // k
 
-get_timestamps_from_video('2_Egg_Vs_95_Egg', model)
+    val_accs = []
+    for i in range(k):
+        train_names = []
+        test_names = []
+        for j in range(i * batches, (i + 1) * batches):
+            test_names.append(names[j])
+        for name in names:
+            if not name in test_names:
+                train_names.append(name)
+
+        train_X, train_Y = [], []
+        for train_name in train_names:
+            X, Y = video_to_data[train_name]
+#             X, Y = load_data_from_video(train_name)
+            train_X += X
+            train_Y += Y
+            del X
+            del Y
+
+        train_X = np.array(train_X)
+        train_Y = np.array(train_Y)
+
+        test_X, test_Y = [], []
+        for test_name in test_names:
+            X, Y = video_to_data[test_name]
+#             X, Y = load_data_from_video(test_name)
+            test_X += X
+            test_Y += Y
+            del X
+            del Y
+
+        test_X = np.array(test_X)
+        test_Y = np.array(test_Y)
+
+        val_acc, size = train_fn(params, str(i), train_X, train_Y, test_X, test_Y)
+        print('max val acc', val_acc)
+        val_accs.append(val_acc)
+        
+        del train_X
+        del train_Y
+        del test_X
+        del test_Y
+        gc.collect()
+    
+    return np.mean(np.array(val_accs)), size
+
+
+# In[ ]:
+
+
+for use_batch_norm in [True, False]:
+    for use_dropout in [True, False]:
+        for layers in [6, 12]:
+            for h in [64]:
+                    
+                params = {
+                    'hidden_units': h,
+                    'use_dropout': use_dropout,
+                    'use_batch_norm': use_batch_norm,
+                    'layers': layers,
+                    'save': False,
+                }
+                f = open('out.txt', 'r')
+                completed = list(f.readlines())
+                if 'Params: ' + str(params) + "\n" in completed:
+                  continue
+                f.close()
+
+                print('params', params)
+                val_acc, size = cross_validate_method(train_nn, params)
+                print(val_acc, size)
+                f = open('out.txt', 'a')
+                f.write('Params: ' + str(params) + '\n')
+                f.write('val acc: ' + str(val_acc) + '\n')
+                f.write('model size: ' + str(size) + '\n')
+                f.close()
 
